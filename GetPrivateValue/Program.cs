ea6514d83ee2233b831c2afe884240690833d809
syuan7492 <kurosaki7492@gmail.com>
2022-04-25 03:05:51 +0800

    Add project files.

---
+++ GetPrivateValue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace GetPrivateValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HasASecret keeper = new HasASecret();
+
+            //Console.WriteLine(keeper.secret);
+
+            //用reflection取得secret欄位的值
+            FieldInfo[] fields = keeper.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (FieldInfo field in fields)
+            {
+                Console.WriteLine(field.GetValue(keeper));
+            }
+        }
+    }
+}
